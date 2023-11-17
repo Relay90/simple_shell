c834819f7884b6c2edca6cae74ddb00e4828401f
Relay90 <motaungr511@gmail.com>
2023-11-17 00:23:34 -0800

    simpli_shell

--- built-in.c
+++ built-in.c
@@ -1,6 +1,27 @@
 #include "shell.h"
 
 /**
+ * exit_shell - This function handles the built-in 'exit' command in the shell.
+ * @args: Array of strings representing the arguments passed
+ * @line: User input buffer.
+ * @env: Environment (list of environment variables).
+ */
+void exit_shell(char **args, char *line, char **env)
+{
+	int i;
+
+	(void)env; /* Avoid unused variable warning */
+
+	for (i = 0; args[i] != NULL; i++)
+	{
+		free(args[i]);
+	}
+	free(args);
+	free(line);
+
+	exit(0);
+}
+/**
  * bridge - Will check to see whether we are dealing with a builtin or not
  * @check: Figures out what to execute
  * @args: Arguments passed from cmdline broken up
